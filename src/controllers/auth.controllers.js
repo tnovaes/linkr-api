d4bf2f7f2625462aa4dc36a8f2f05d5a351da793
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-30 17:40:16 -0300

    chore: add signIn controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,17 +1,34 @@
+import jwt from "jsonwebtoken";
 import { createUser, getUserByEmail } from "../repositories/user.repository.js";
 import bcrypt from "bcrypt";
+import { createSession } from "../repositories/session.repository.js";
 
 export async function signUp(req, res) {
     const { name, email, password, avatar } = req.body;
 
     try {
-        const user = getUserByEmail(email);
+        const user = await getUserByEmail(email);
         if (user.rowCount) return res.status(400).send({ message: "Email already registered." });
 
         const hash = bcrypt.hashSync(password, 10);
         await createUser(name, email, hash, avatar);
 
         res.sendStatus(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+export async function signIn(req, res) {
+    const { email, password } = req.body;
+    try {
+        const user =  await getUserByEmail(email);
+        if (user.rowCount===0) return res.status(401).send({ message: "Unauthorized" });
+        const user_id = user.rows[0].id;
+        const isValidPassword = bcrypt.compareSync(password, user.rows[0].password);
+        if (!isValidPassword) return res.status(401).send({ message: "Unauthorized" });
+        const token = jwt.sign({id: user_id}, process.env.JWT_PASSWORD);
+        createSession(token, user_id);
+        res.send({token});
     } catch (err) {
         res.status(500).send(err.message);
     }

fe32688485af4c39c59632efcc9e25fedcea2ca7
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-06-01 14:56:12 -0300

    feat: getUserInfoByID

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,3 +1,4 @@
+import { getPostsByUserIDDB } from "../repositories/posts.repository.js";
 import { getUsersBySearchTextDB } from "../repositories/user.repository.js";
 
 export async function getUsersBySearchText(req, res) {
@@ -6,6 +7,16 @@ export async function getUsersBySearchText(req, res) {
     try {
         const users = await getUsersBySearchTextDB(id,searchText)
         res.send(users.rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function getPostsByUserID(req, res) {
+    try {
+    const {id} = req.params
+    const user = await getPostsByUserIDDB(id)
+    res.send(user.rows[0])
     } catch (err) {
         res.status(500).send(err.message);
     }

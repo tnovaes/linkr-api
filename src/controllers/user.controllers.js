500f8b2d11bc635e18c48104813d9e8eb50c38a0
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-06-02 15:00:42 -0300

    refactor: change name from getUserByID to getPostsByUserID

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,9 +3,9 @@ import { getUsersBySearchTextDB } from "../repositories/user.repository.js";
 
 export async function getUsersBySearchText(req, res) {
     const { id } = req.tokenData;
-    const {searchText} = req.body
+    const { searchText } = req.body
     try {
-        const users = await getUsersBySearchTextDB(id,searchText)
+        const users = await getUsersBySearchTextDB(id, searchText)
         res.send(users.rows);
     } catch (err) {
         res.status(500).send(err.message);
@@ -14,9 +14,9 @@ export async function getUsersBySearchText(req, res) {
 
 export async function getPostsByUserID(req, res) {
     try {
-    const {id} = req.params
-    const user = await getPostsByUserIDDB(id)
-    res.send(user.rows[0])
+        const { id } = req.params
+        const user = await getPostsByUserIDDB(id)
+        res.send(user.rows[0])
     } catch (err) {
         res.status(500).send(err.message);
     }

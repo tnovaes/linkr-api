1dee96c01fedcfb9390d2a42b6fd003685b5babd
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-06-02 08:59:53 -0300

    fix: add limit of 20 and orderBy date insertion

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -1,7 +1,13 @@
 import { db } from "../database/database.connection.js";
 
 export function getPostsByUserIDDB(id) {
-    return db.query(`SELECT u.name, u.avatar, json_agg(json_build_object('shared_link', p.shared_link, 'description', p.description)) as posts FROM posts p LEFT JOIN users u on p.user_id=u.id WHERE p.user_id=$1 GROUP BY u.name, u.avatar`, [id]);
+    return db.query(`SELECT u.name, u.avatar, json_agg(json_build_object('shared_link', p.shared_link, 'description', p.description)) as posts 
+    FROM posts p 
+    LEFT JOIN users u on p.user_id=u.id 
+    WHERE p.user_id=$1 
+    GROUP BY u.name, u.avatar, p.created_at 
+    ORDER BY p.created_at DESC
+    LIMIT 20`, [id]);
 }
 
 export function insertPost(user_id, shared_link, description) {
